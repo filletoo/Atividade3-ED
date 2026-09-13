662c915ac857aa4e453d4309e7e91276c4f82e1f
Mandrake <felipebfsanches0@gmail.com>
2026-09-13 18:30:52 -0300

    Mudanças que fiz no Atendimento
    Essas mudanças são as que adequam ao meu codigo do fila_propriedade_array_2

--- atendimento.py
+++ atendimento.py
@@ -1,20 +1,7 @@
-from fila_prioridade_array_2 import *
+from fila_propriedade_array_2 import *
 
-def mostrar_estatisticas(atendidas):
-    com_prio = 0
-    sem_prio = 0
-    total = len(atendidas)
-
-    for p in atendidas:
-        if p.prioridade: com_prio += 1
-        else: sem_prio += 1
-
-    print(f"Total de pessoas atendidas: {total}")
-    print(f"Percentual de atendimentos com prioridade: %{com_prio/total*100:.2f}")
-    print(f"Percentual de atendimentos sem prioridade: %{sem_prio/total*100:.2f}")
-
-fila = FilaPrioridadeArray()
-
+fila_p = FilaPrioridadeArray()
+fila_n = FilaPrioridadeArray()
 menu = '''-------------------------
 1. Chegada de pessoa para atendimento
 2. Realizar atendimento
@@ -24,64 +11,80 @@ menu = '''-------------------------
 Opção: '''
 
 atender_prioridade = False
-pessoas_atendidas = []
-
+atendidos = 0
+atendidos_prioridade = 0
 while True:
     opcao = input(menu)
 
     if opcao == '1':
         nome = input("Indique o nome da pessoa: ")
         
-        '''while True:
+        while True:
             prioridade = input("Ela tem prioridade? (s/n)").lower()
             if prioridade not in "sn":
                 print("Indique uma opção válida")
             else:
-                if prioridade == 's': prioridade = True
-                else: prioridade = False
+                if prioridade == 's': 
+                    prioridade = True
+                else: 
+                    prioridade = False
+                if prioridade:
+                    fila_p.enqueue(PessoaNaFila(nome))
+                else:
+                    fila_n.enqueue(PessoaNaFila(nome))
                 break
-        '''
-        if '*' in nome:
-            prioridade = True
+            
+            
+
+    if opcao == '2':  # Dequeue de acordo com a política 2 sem 1 com
+        # Verifica se as duas filas estão vazias
+        if fila_p.isEmpty() and fila_n.isEmpty():
+            print("Não há pessoas na fila para atendimento.")
+
+        # A cada 3 atendimentos, tenta atender prioridade
+        elif atendidos % 3 == 2:
+
+            if not fila_p.isEmpty():
+                print(f"Atendendo pessoa com prioridade: {fila_p.dequeue()}")
+                atendidos += 1
+                atendidos_prioridade += 1
+
+            elif not fila_n.isEmpty():
+                print(f"Fila de prioridade vazia.")
+                print(f"Atendendo pessoa sem prioridade: {fila_n.dequeue()}")
+                atendidos += 1
+
+        # Nos outros dois atendimentos, tenta atender sem prioridade
         else:
-            prioridade = False
 
-        pessoa = PessoaNaFila(nome, prioridade)
-        fila.enqueue(pessoa)
+            if not fila_n.isEmpty():
+                print(f"Atendendo pessoa sem prioridade: {fila_n.dequeue()}")
+                atendidos += 1
 
-    if opcao == '2':
-        if fila.isEmpty():
-            print("Não há mais pessoas para atender na fila")
-            continue
+            elif not fila_p.isEmpty():
+                print(f"Fila sem prioridade vazia.")
+                print(f"Atendendo pessoa com prioridade: {fila_p.dequeue()}")
+                atendidos += 1
+                atendidos_prioridade += 1
 
-        atender = fila.dequeue(atender_prioridade)
+        
 
-        if atender[0].prioridade == atender_prioridade:
-            atender_prioridade = not atender_prioridade
-
-        for i in range(len(atender)):
-            if atender[i] != '':
-                pessoas_atendidas.append(atender[i])
- 
-    if opcao == '3':
+    if opcao == '3': 
         print("Pessoas com prioridade:")
-        for i in range(fila.fim + 1):
-            if fila.fila[i] != '':
-                if fila.fila[i].prioridade:
-                    print(fila.fila[i].nome)
-
-        print()
+        fila_p.PrintLista()
         print("Pessoas sem prioridade:")
-        for i in range(fila.fim + 1):
-            if fila.fila[i] != '':
-                if not fila.fila[i].prioridade:
-                    print(fila.fila[i].nome)
-
+        fila_n.PrintLista()
     if opcao == '4':
-        if not fila.isEmpty():
-            print("Você não pode sair agora, ainda há pessoas na fila")
-        else: 
-           
+        if fila_p.isEmpty() and fila_n.isEmpty():
+            print (f"Total de pessoas atendidas: {atendidos}")
+            if atendidos > 0:
+                percentual_prioridade = (atendidos_prioridade / atendidos) * 100
+                percentual_sem_prioridade = 100 - percentual_prioridade
+                print(f"Percentual de atendimentos com prioridade: {percentual_prioridade:.2f}%")
+                print(f"Percentual de atendimentos sem prioridade: {percentual_sem_prioridade:.2f}%")
             break
-
-mostrar_estatisticas(pessoas_atendidas)
+        else:
+            print("Ainda existem pessoas na fila. Não é possível sair.")
+        
+    if opcao not in ['1', '2', '3', '4']:
+        print("Opção inválida. Por favor, tente novamente.")
