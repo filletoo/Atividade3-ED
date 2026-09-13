907ea617ec15e79ead198938eb9f356eabcd49ae
Mandrake <felipebfsanches0@gmail.com>
2026-09-13 18:26:40 -0300

    Mudanças de Felipe
    Mudanças que junto a elas deve-se fazer no atendimento.py

--- fila_prioridade_array_2.py
+++ fila_prioridade_array_2.py
@@ -1,79 +1,60 @@
 class PessoaNaFila:
-    def __init__(self, nome, prioridade):
-        self.nome = nome
+    def __init__(self, prioridade):
         self.prioridade = prioridade
+    def __str__(self):
+        return self.prioridade
 
 class FilaPrioridadeArray:
     def __init__(self):
         #tamanhos
-        self.tam = 20
+        self.tam = 2
         #filas com e sem prioridade
         self.fila = ['']*self.tam
         #indicadores
-        self.quant = 0
+        self.inicio = 0
         self.fim = -1
+        self.quant = 0
+        
 
     #redimensionar o array
     def redimensionar_fila(self):
-        tam = self.tam
-        array = self.fila
-        novo = ['']*tam
+        nova_fila = ['']*self.tam
         j = 0
-        for i in range(self.fim + 1):
-            if array[i] != '':
-                novo[j] = array[i]
+        for i in range(self.inicio, self.fim + 1):
+            if self.fila[i] != '':
+                nova_fila[j] = self.fila[i]
                 j += 1
-
-        self.fim = self.tam - 1
-        return novo
+        self.fila = nova_fila
+        self.inicio = 0
+        self.fim = j - 1
     
     def enqueue(self, pessoa):
         #ver se precisa redimensionar
-        if self.fim == self.tam - 1:
+        if self.quant == self.tam:
             self.tam *= 2
             self.redimensionar_fila()
-
         self.fila[self.fim + 1] = pessoa
         self.fim += 1
         self.quant += 1
-
-    def dequeue(self, prioridade):
-        if self.isEmpty(): return False
-
-        com_prio = sem_prio = 0
-        for i in range(self.fim + 1):
-            if self.fila[i] != '':
-                if self.fila[i].prioridade:
-                    com_prio += 1
-                else:
-                    sem_prio += 1
-
-        #ver se precisa redimensionar
-        if self.quant == self.tam//4:
+        return pessoa
+    
+    def dequeue(self):
+        if self.isEmpty():
+            raise IndexError("A fila está vazia")
+        retorno = self.fila[self.inicio]
+        self.fila[self.inicio] = ''
+        self.inicio += 1
+        self.quant -= 1
+        # Redimensionar depois da remoção
+        if self.quant <= self.tam // 4 and self.tam > 2:
             self.tam //= 2
             self.redimensionar_fila()
-            self.fim = self.quant - 1
-
-        if (prioridade and com_prio != 0) or sem_prio == 0:
-            atender = ['']
-            for i in range(self.fim + 1):
-                if self.fila[i] != '' and self.fila[i].prioridade:
-                    atender[0] = self.fila[i]
-                    self.fila[i] = ''
-                    self.quant -= 1
-                    break
-        
-        if (not prioridade and sem_prio != 0) or com_prio == 0:
-            atender = ['']*2
-            j = 0
-            for i in range(self.fim + 1):
-                if self.fila[i] != '' and not self.fila[i].prioridade:
-                    atender[j] = self.fila[i]
-                    self.fila[i] = ''
-                    j += 1
-                    self.quant -= 1
-                if j == 2: break
-        return atender
-
+        return retorno
+    
     def isEmpty(self):
-        return (self.quant == 0)
+        return self.quant == 0
+    
+    def PrintLista(self): 
+        for i in range(self.inicio, self.fim + 1):
+            if self.fila[i] != '':
+                print(self.fila[i])
