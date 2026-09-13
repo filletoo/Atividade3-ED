53942594af6eaa779cc0d9d13c265f76dbf053f4
Carlos <carloseduardosouzaoliveira95@gmail.com>
2026-09-13 18:00:20 -0300

    Add files via upload

--- atendimento.py
+++ atendimento.py
@@ -1,4 +1,17 @@
-from fila_prioridade_array import *
+from fila_prioridade_array_2 import *
+
+def mostrar_estatisticas(atendidas):
+    com_prio = 0
+    sem_prio = 0
+    total = len(atendidas)
+
+    for p in atendidas:
+        if p.prioridade: com_prio += 1
+        else: sem_prio += 1
+
+    print(f"Total de pessoas atendidas: {total}")
+    print(f"Percentual de atendimentos com prioridade: %{com_prio/total*100:.2f}")
+    print(f"Percentual de atendimentos sem prioridade: %{sem_prio/total*100:.2f}")
 
 fila = FilaPrioridadeArray()
 
@@ -11,6 +24,7 @@ menu = '''-------------------------
 Opção: '''
 
 atender_prioridade = False
+pessoas_atendidas = []
 
 while True:
     opcao = input(menu)
@@ -18,7 +32,7 @@ while True:
     if opcao == '1':
         nome = input("Indique o nome da pessoa: ")
         
-        while True:
+        '''while True:
             prioridade = input("Ela tem prioridade? (s/n)").lower()
             if prioridade not in "sn":
                 print("Indique uma opção válida")
@@ -26,20 +40,48 @@ while True:
                 if prioridade == 's': prioridade = True
                 else: prioridade = False
                 break
+        '''
+        if '*' in nome:
+            prioridade = True
+        else:
+            prioridade = False
 
-        fila.enqueue(nome, prioridade)
+        pessoa = PessoaNaFila(nome, prioridade)
+        fila.enqueue(pessoa)
 
     if opcao == '2':
-        fila.dequeue(atender_prioridade)
-        atender_prioridade = not atender_prioridade
+        if fila.isEmpty():
+            print("Não há mais pessoas para atender na fila")
+            continue
 
+        atender = fila.dequeue(atender_prioridade)
+
+        if atender[0].prioridade == atender_prioridade:
+            atender_prioridade = not atender_prioridade
+
+        for i in range(len(atender)):
+            if atender[i] != '':
+                pessoas_atendidas.append(atender[i])
+ 
     if opcao == '3':
         print("Pessoas com prioridade:")
-        for i in range(fila.i_prio, fila.f_prio):
-            print(fila.fila_prio[i])
+        for i in range(fila.fim + 1):
+            if fila.fila[i] != '':
+                if fila.fila[i].prioridade:
+                    print(fila.fila[i].nome)
 
         print()
         print("Pessoas sem prioridade:")
-        for i in range(fila.i_normal, fila.f_normal):
-            print(fila.fila_normal[i])
-        print()
+        for i in range(fila.fim + 1):
+            if fila.fila[i] != '':
+                if not fila.fila[i].prioridade:
+                    print(fila.fila[i].nome)
+
+    if opcao == '4':
+        if not fila.isEmpty():
+            print("Você não pode sair agora, ainda há pessoas na fila")
+        else: 
+           
+            break
+
+mostrar_estatisticas(pessoas_atendidas)
--- fila_prioridade_array_2.py
+++ fila_prioridade_array_2.py
@@ -1,15 +1,16 @@
 class PessoaNaFila:
-    def __init__(self, prioridade):
+    def __init__(self, nome, prioridade):
+        self.nome = nome
         self.prioridade = prioridade
 
 class FilaPrioridadeArray:
     def __init__(self):
         #tamanhos
-        self.tam = 10
+        self.tam = 20
         #filas com e sem prioridade
         self.fila = ['']*self.tam
         #indicadores
-        self.inicio = -1
+        self.quant = 0
         self.fim = -1
 
     #redimensionar o array
@@ -23,6 +24,7 @@ class FilaPrioridadeArray:
                 novo[j] = array[i]
                 j += 1
 
+        self.fim = self.tam - 1
         return novo
     
     def enqueue(self, pessoa):
@@ -33,10 +35,12 @@ class FilaPrioridadeArray:
 
         self.fila[self.fim + 1] = pessoa
         self.fim += 1
+        self.quant += 1
 
     def dequeue(self, prioridade):
         if self.isEmpty(): return False
-        com_prio, sem_prio = 0
+
+        com_prio = sem_prio = 0
         for i in range(self.fim + 1):
             if self.fila[i] != '':
                 if self.fila[i].prioridade:
@@ -48,25 +52,28 @@ class FilaPrioridadeArray:
         if self.quant == self.tam//4:
             self.tam //= 2
             self.redimensionar_fila()
+            self.fim = self.quant - 1
 
         if (prioridade and com_prio != 0) or sem_prio == 0:
             atender = ['']
-            for i in range(self.inicio, self.fim + 1):
+            for i in range(self.fim + 1):
                 if self.fila[i] != '' and self.fila[i].prioridade:
                     atender[0] = self.fila[i]
                     self.fila[i] = ''
+                    self.quant -= 1
                     break
         
         if (not prioridade and sem_prio != 0) or com_prio == 0:
             atender = ['']*2
             j = 0
-            for i in range(self.inicio, self.fim + 1):
+            for i in range(self.fim + 1):
                 if self.fila[i] != '' and not self.fila[i].prioridade:
                     atender[j] = self.fila[i]
                     self.fila[i] = ''
                     j += 1
+                    self.quant -= 1
                 if j == 2: break
         return atender
 
     def isEmpty(self):
-        return (self.inicio == self.fim)
+        return (self.quant == 0)
